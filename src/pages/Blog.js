5c199d9ee10fc0b75610db0f0cc9a117e69a2517
Alex <aspindle@ucsd.edu>
2022-07-28 20:43:19 -0700

    add ideas and plans

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -12,6 +12,8 @@ export default function Blog() {
         <br />
         <a href="https://docs.google.com/document/d/1Xu0NMSasV6-XCsrdHGqUaokclvnWWDViLrzdO1kQwfk/edit?usp=sharing">Goals</a>
         <br />
+        <a href="https://docs.google.com/document/d/11qA5EAtMH72luflTaVAydSjEc-e6a4sF-BB5M4WYqW4/edit?usp=sharing">Ideas and Plans</a>
+        <br />
         </>
     )
 }
