9f12580f18dc11235fef3b9f11c225af096fd075
Alex <aspindle@ucsd.edu>
2022-08-19 21:38:28 -0700

    add old stuff to blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -6,13 +6,19 @@ export default function Blog() {
         <>
         <Link to="/">Back</Link>
         <h1>Blog</h1>
-        <a href="https://www.youtube.com/watch?v=CIjnzXxtM-A">Lord of the Most Dangerous Hunger Games</a>
+        <a href="https://docs.google.com/document/d/1MC6Byz5m-GtigwsfVtnOg5EXPs7IBktm/edit?usp=sharing&ouid=102108365647779910091&rtpof=true&sd=true">AP Chemistry Spring Project (2012)</a>
         <br />
-        <a href="https://docs.google.com/document/d/1_Mu392CzJ7lmZg-PQ3br3ZxXzCbIN6y3To-EAXGBVlE/edit?usp=sharing">Math in Rome</a>
+        <a href="https://docs.google.com/document/d/18lHsKV0ZF7JKjSAXw48vsXq_KOwvYSor/edit?usp=sharing&ouid=102108365647779910091&rtpof=true&sd=true">Eagle Scout Project Report (2013)</a>
+        <br />
+        <a href="https://www.youtube.com/watch?v=CIjnzXxtM-A">Lord of the Most Dangerous Hunger Games - AP Language Arts (2013)</a>
+        <br />
+        <a href="https://docs.google.com/document/d/16UtD1-13AHWUUAywWaW--OB_L2l--SoX/edit?usp=sharing&ouid=102108365647779910091&rtpof=true&sd=true">The Apollo Controversy - AP US History Final Research Essay (2013)</a>
+        <br />
+        <a href="https://docs.google.com/document/d/1_Mu392CzJ7lmZg-PQ3br3ZxXzCbIN6y3To-EAXGBVlE/edit?usp=sharing">Math in Rome (2015)</a>
         <br />
         <a href="https://docs.google.com/document/d/1Xu0NMSasV6-XCsrdHGqUaokclvnWWDViLrzdO1kQwfk/edit?usp=sharing">Goals</a>
         <br />
-        <a href="https://docs.google.com/document/d/11qA5EAtMH72luflTaVAydSjEc-e6a4sF-BB5M4WYqW4/edit?usp=sharing">Ideas and Plans</a>
+        <a href="https://docs.google.com/document/d/11qA5EAtMH72luflTaVAydSjEc-e6a4sF-BB5M4WYqW4/edit?usp=sharing">Ideas and Plans for Spindler Academy</a>
         <br />
         </>
     )
