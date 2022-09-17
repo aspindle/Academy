d7d64cd639a35655495639d1d01226bcf0a068d2
Alex <aspindle@ucsd.edu>
2022-09-16 21:30:21 -0700

    remove goals from blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -28,8 +28,8 @@ export default function Blog() {
         <a href="https://link.medium.com/cQzeBpuPatb">On Death... (2022)</a>
         <br /> */}
         
-        <a href="https://docs.google.com/document/d/1Xu0NMSasV6-XCsrdHGqUaokclvnWWDViLrzdO1kQwfk/edit?usp=sharing">Goals</a>
-        <br />
+        {/*<a href="https://docs.google.com/document/d/1Xu0NMSasV6-XCsrdHGqUaokclvnWWDViLrzdO1kQwfk/edit?usp=sharing">Goals</a>
+        <br />*/}
         <a href="https://docs.google.com/document/d/11qA5EAtMH72luflTaVAydSjEc-e6a4sF-BB5M4WYqW4/edit?usp=sharing">Ideas and Plans for Spindler Academy</a>
         <br />
         </>
