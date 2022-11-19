3faa70029718617740f6383a9c03a6307668ac7f
Alex <aspindle@ucsd.edu>
2022-11-19 15:52:19 -0800

    add spacing to blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -5,33 +5,38 @@ export default function Blog() {
     return (
         <>
         <Navbar/>
-        <h1>Blog</h1>
-        <a href="https://www.dropbox.com/s/6dc5k0w5crcuttp/IMG_2332.mov?dl=0">Bending Animation (before 2010)</a>
-        <br />
-        <a href="https://docs.google.com/document/d/1MC6Byz5m-GtigwsfVtnOg5EXPs7IBktm/edit?usp=sharing&ouid=102108365647779910091&rtpof=true&sd=true">AP Chemistry Spring Project (2012)</a>
-        <br />
-        <a href="https://docs.google.com/document/d/18lHsKV0ZF7JKjSAXw48vsXq_KOwvYSor/edit?usp=sharing&ouid=102108365647779910091&rtpof=true&sd=true">Eagle Scout Project Report (2013)</a>
-        <br />
-        <a href="https://www.youtube.com/watch?v=CIjnzXxtM-A">Lord of the Most Dangerous Hunger Games - AP Language Arts (2013)</a>
-        <br />
-        <a href="https://docs.google.com/document/d/16UtD1-13AHWUUAywWaW--OB_L2l--SoX/edit?usp=sharing&ouid=102108365647779910091&rtpof=true&sd=true">The Apollo Controversy - AP US History Final Research Essay (2013)</a>
-        <br />
-        <a href="https://www.dropbox.com/s/9s3dcnh5cwt9b73/Essay%202%20Final%20Draft.docx?dl=0">Essay on The Penultimate Truth and Society of the Spectacle (2015)</a>
-        <br />
-        <a href="https://docs.google.com/document/d/1_Mu392CzJ7lmZg-PQ3br3ZxXzCbIN6y3To-EAXGBVlE/edit?usp=sharing">Math in Rome (2015)</a>
-        <br />
-        <a href="https://dogebook.netlify.app/">Dogebook (2021)</a>
-        <br />
-        <a href="https://youtu.be/2qQ1M0sTAEY">Alaska Cruise Vlog (2022)</a>
-        <br />
+
+        <div className='bdiv'>
+            <h1>Blog</h1>
+            <a href="https://www.dropbox.com/s/6dc5k0w5crcuttp/IMG_2332.mov?dl=0">Bending Animation (before 2010)</a>
+            <br /><br />
+            <a href="https://docs.google.com/document/d/1MC6Byz5m-GtigwsfVtnOg5EXPs7IBktm/edit?usp=sharing&ouid=102108365647779910091&rtpof=true&sd=true">AP Chemistry Spring Project (2012)</a>
+            <br /><br />
+            <a href="https://docs.google.com/document/d/18lHsKV0ZF7JKjSAXw48vsXq_KOwvYSor/edit?usp=sharing&ouid=102108365647779910091&rtpof=true&sd=true">Eagle Scout Project Report (2013)</a>
+            <br /><br />
+            <a href="https://www.youtube.com/watch?v=CIjnzXxtM-A">Lord of the Most Dangerous Hunger Games - AP Language Arts (2013)</a>
+            <br /><br />
+            <a href="https://docs.google.com/document/d/16UtD1-13AHWUUAywWaW--OB_L2l--SoX/edit?usp=sharing&ouid=102108365647779910091&rtpof=true&sd=true">The Apollo Controversy - AP US History Final Research Essay (2013)</a>
+            <br /><br />
+            <a href="https://www.dropbox.com/s/9s3dcnh5cwt9b73/Essay%202%20Final%20Draft.docx?dl=0">Essay on The Penultimate Truth and Society of the Spectacle (2015)</a>
+            <br /><br />
+            <a href="https://docs.google.com/document/d/1_Mu392CzJ7lmZg-PQ3br3ZxXzCbIN6y3To-EAXGBVlE/edit?usp=sharing">Math in Rome (2015)</a>
+            <br /><br />
+            <a href="https://dogebook.netlify.app/">Dogebook (2021)</a>
+            <br /><br />
+            <a href="https://youtu.be/2qQ1M0sTAEY">Alaska Cruise Vlog (2022)</a>
+            <br /><br />
+            <a href="https://docs.google.com/document/d/11qA5EAtMH72luflTaVAydSjEc-e6a4sF-BB5M4WYqW4/edit?usp=sharing">Ideas and Plans for Spindler Academy</a>
+            <br />
+        </div>
+        
         {/*
         <a href="https://link.medium.com/cQzeBpuPatb">On Death... (2022)</a>
         <br /> */}
         
         {/*<a href="https://docs.google.com/document/d/1Xu0NMSasV6-XCsrdHGqUaokclvnWWDViLrzdO1kQwfk/edit?usp=sharing">Goals</a>
         <br />*/}
-        <a href="https://docs.google.com/document/d/11qA5EAtMH72luflTaVAydSjEc-e6a4sF-BB5M4WYqW4/edit?usp=sharing">Ideas and Plans for Spindler Academy</a>
-        <br />
+        
         </>
     )
 }
